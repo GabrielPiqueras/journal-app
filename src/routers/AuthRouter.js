3900d0def53639e34798afbd58300222143d88d7
Gabriel Piqueras <rocamora_gabriel@hotmail.com>
2022-06-28 00:07:19 +0200

    Inicio de aplicación.

---
+++ src/routers/AuthRouter.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { LoginPage } from '../components/auth/LoginPage';
+import { RegisterPage } from '../components/auth/RegisterPage';
+
+export const AuthRouter = () => {
+  return (
+      <Routes>
+        <Route exact path='/auth/login' element={ <LoginPage /> } />
+        <Route exact path='auth/register' element={ <RegisterPage /> } />
+        <Route path='*' element={ <Navigate to='/auth/login' replace /> } />
+      </Routes>
+  )
+} 
