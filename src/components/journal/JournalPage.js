ce3f00684df355a8318449b4ea328d8646815246
Gabriel Piqueras <rocamora_gabriel@hotmail.com>
2022-06-30 01:19:31 +0200

    journal

--- src/components/journal/JournalPage.js
+++ src/components/journal/JournalPage.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Sidebar } from './Sidebar'
 
 export const JournalPage = () => {
   return (
-    <div>
+    <div className='journal__main-content'>
         <h1>JournalPage</h1>
+        
+        <Sidebar />
+
+        <main>
+          <h1>Main content</h1>
+        </main>
     </div>
   )
 }
