83c6d4189ce405ffcc8990bd23071a9694b66f5d
Gabriel Piqueras <rocamora_gabriel@hotmail.com>
2022-06-30 01:19:47 +0200

    l

---
+++ src/components/journal/JournalEntry.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+export const JournalEntry = () => {
+    return (
+        <div className="journal__entry pointer">
+            
+            <div 
+                className="journal__entry-picture"
+                style={{
+                    backgroundSize: 'cover',
+                    backgroundImage: 'url(https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Zm9jdXN8ZW58MHx8MHx8&w=1000&q=80)'
+                }}
+            ></div>
+
+            <div className="journal__entry-body">
+                <p className="journal__entry-title">
+                    Un nuevo día
+                </p>
+                <p className="journal__entry-content">
+                    Reprehenderit id in duis consectetur deserunt veniam fugiat.
+                </p>
+            </div>
+
+            <div className="journal__entry-date-box">
+                <span>Monday</span>
+                <h4>28</h4>
+            </div>
+
+        </div>
+    )
+}
