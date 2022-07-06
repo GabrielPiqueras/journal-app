3a5ebc39a97f9b3b93f2bdf00701c61ad924387e
gabriel_inf24 <gabriel@informatica24.com>
2022-07-06 15:12:33 +0200

    Fallo corregido al exportar en config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -13,10 +13,4 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-export {
-  db,
-  signInWithPopup,
-  googleAuthProvider
-}
+export const db = getFirestore(app);
